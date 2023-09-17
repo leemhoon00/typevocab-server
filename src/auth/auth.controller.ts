521d8a872f0ace9b9e6646516631fe4b66a68298
leemhoon00 <leemhoon00@naver.com>
2023-09-18 00:17:10 +0900

    auth controller httpcode

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards, HttpCode } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
@@ -19,6 +19,7 @@ export class AuthController {
   })
   @Get('kakao')
   @UseGuards(AuthGuard('kakao'))
+  @HttpCode(301)
   kakaoRedirect(@Req() req: Request, @Res() res: Response) {
     return this.authService.kakaoLogin(req.user.id, res);
   }
@@ -28,6 +29,7 @@ export class AuthController {
     description: '홈으로 리다이렉트',
   })
   @Get('logout')
+  @HttpCode(301)
   logout(@Res() res: Response) {
     return this.authService.logout(res);
   }

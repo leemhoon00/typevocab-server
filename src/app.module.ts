24ee2aa23f7100388c64bc3e87f9088eeec8e077
leemhoon00 <leemhoon00@naver.com>
2023-09-26 17:06:42 +0900

    define prisma model

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,27 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { FoldersModule } from './folders/folders.module';
 import { VocabulariesModule } from './vocabularies/vocabularies.module';
 import { WordsModule } from './words/words.module';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-          expandVariables: true,
-          cache: true,
-        }),
-      ],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URL'),
-      }),
-      inject: [ConfigService],
-    }),
     AuthModule,
     UsersModule,
     FoldersModule,
